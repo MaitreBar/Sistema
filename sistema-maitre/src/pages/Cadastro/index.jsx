d573a4f233bbf7825357f589c96dc3e1521d0487
cesarLimaz29 <cesa.de.sousa.lima@accenture.com>
2023-04-13 00:42:43 -0300

    adicionando alguns arquivos

--- sistema-maitre/src/pages/Cadastro/index.jsx
+++ sistema-maitre/src/pages/Cadastro/index.jsx
@@ -2,47 +2,18 @@ import './style.css';
 
 function Cadastro() {
     return (
-        <div class="containerzao">
+        <div>
 
-            <div className="banner">
-                
-
-
-
-
+            <div className='cadastrese'>
+            
+            <div className='inputs'>
+                    <p> inputs here</p>
+                </div>
             </div>
-
-
-            <div class="container-inputs">
-
-
-                <h1> Cadastre - Se</h1>
-             
-                <p> Insira os dados abaixo:</p>
-
-                <div class="single-input">
-
-                <input type="text" placeholder='Nome Completo'></input>
-                <input type="text" placeholder='CPF'></input>
-                <input type="text" placeholder='RG'></input>
-                <input type="text" placeholder='Data de Nascimento'></input>
-                <input type="text" placeholder='E-mail'></input>
-                <input type="text" placeholder='Telefone'></input>
-                
-
+            <div className='banner'>
+            <p>foto</p>
             </div>
-
-
-                <span class="msg-baixo"> Já possui conta ? <a href="login.html">Faça login</a></span>
-                <button class="button-28">Cadastrar</button>
-            </div>
-
         </div>
-
-
-
-
-
     );
 }
 
