17740e37c035a7bb1508093538efa59085755f0f
cesarLimaz29 <cesa.de.sousa.lima@accenture.com>
2023-04-24 21:29:18 -0300

    adicionando algumas coisas

---
+++ sistema-maitre/src/pages/Catalogo/index.jsx
@@ -0,0 +1,142 @@
+import React from 'react';
+import Navbar from '../navbar';
+import imagem from '../../assets/banner/bannercard.png';
+
+import './catalogo.css';
+function Catalogo() {
+    return (
+        <div className='body'>
+            <Navbar />
+
+            <div className="container">
+
+                <div className="content">
+                    <img src={imagem} alt="Imagem" />
+                    <div className="text">
+                        <span className='negrito'>Restaurantes na região</span>
+                        <p>Rua Rosa Nova,123</p>
+                    </div>
+                    <div className='text-2'>
+                        <span className='negrito'>Faixa de Preço</span>
+                        <p>de 100R$ a 300R$</p>
+                    </div>
+                </div>
+
+                <div className="content">
+                    <img src={imagem} alt="Imagem" />
+                    <div className="text">
+                        <span className='negrito'>Restaurantes na região</span>
+                        <p>Rua Rosa Nova,123</p>
+                    </div>
+                    <div className='text-2'>
+                        <span className='negrito'>Faixa de Preço</span>
+                        <p>de 100R$ a 300R$</p>
+                    </div>
+                </div>
+                
+                <div className="content">
+                    <img src={imagem} alt="Imagem" />
+                    <div className="text">
+                        <span className='negrito'>Restaurantes na região</span>
+                        <p>Rua Rosa Nova,123</p>
+                    </div>
+                    <div className='text-2'>
+                        <span className='negrito'>Faixa de Preço</span>
+                        <p>de 100R$ a 300R$</p>
+                    </div>
+                </div>
+
+                <div className="content">
+                    <img src={imagem} alt="Imagem" />
+                    <div className="text">
+                        <span className='negrito'>Restaurantes na região</span>
+                        <p>Rua Rosa Nova,123</p>
+                    </div>
+                    <div className='text-2'>
+                        <span className='negrito'>Faixa de Preço</span>
+                        <p>de 100R$ a 300R$</p>
+                    </div>
+                </div>
+
+                <div className="content">
+                    <img src={imagem} alt="Imagem" />
+                    <div className="text">
+                        <span className='negrito'>Restaurantes na região</span>
+                        <p>Rua Rosa Nova,123</p>
+                    </div>
+                    <div className='text-2'>
+                        <span className='negrito'>Faixa de Preço</span>
+                        <p>de 100R$ a 300R$</p>
+                    </div>
+                </div>
+
+                <div className="content">
+                    <img src={imagem} alt="Imagem" />
+                    <div className="text">
+                        <span className='negrito'>Restaurantes na região</span>
+                        <p>Rua Rosa Nova,123</p>
+                    </div>
+                    <div className='text-2'>
+                        <span className='negrito'>Faixa de Preço</span>
+                        <p>de 100R$ a 300R$</p>
+                    </div>
+                </div>
+
+                <div className="content">
+                    <img src={imagem} alt="Imagem" />
+                    <div className="text">
+                        <span className='negrito'>Restaurantes na região</span>
+                        <p>Rua Rosa Nova,123</p>
+                    </div>
+                    <div className='text-2'>
+                        <span className='negrito'>Faixa de Preço</span>
+                        <p>de 100R$ a 300R$</p>
+                    </div>
+                </div>
+
+                <div className="content">
+                    <img src={imagem} alt="Imagem" />
+                    <div className="text">
+                        <span className='negrito'>Restaurantes na região</span>
+                        <p>Rua Rosa Nova,123</p>
+                    </div>
+                    <div className='text-2'>
+                        <span className='negrito'>Faixa de Preço</span>
+                        <p>de 100R$ a 300R$</p>
+                    </div>
+                </div>
+
+                <div className="content">
+                    <img src={imagem} alt="Imagem" />
+                    <div className="text">
+                        <span className='negrito'>Restaurantes na região</span>
+                        <p>Rua Rosa Nova,123</p>
+                    </div>
+                    <div className='text-2'>
+                        <span className='negrito'>Faixa de Preço</span>
+                        <p>de 100R$ a 300R$</p>
+                    </div>
+                </div>
+
+                <div className="content">
+                    <img src={imagem} alt="Imagem" />
+                    <div className="text">
+                        <span className='negrito'>Restaurantes na região</span>
+                        <p>Rua Rosa Nova,123</p>
+                    </div>
+                    <div className='text-2'>
+                        <span className='negrito'>Faixa de Preço</span>
+                        <p>de 100R$ a 300R$</p>
+                    </div>
+                    <span className='tags'>Petiscos</span>
+                    <span className='tags'> Forró </span>
+                    <span className='tags'> Bebidas não alcoólicas</span>
+                </div>
+
+
+
+            </div>
+        </div>
+    );
+}
+export default Catalogo;
