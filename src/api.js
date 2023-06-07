b108f1f0cb6b2bbdcfbbbb78ef636f2482e73e0e
landineo <lucas.rodrigues@sptech.school>
2023-06-06 23:54:24 -0300

    apagando pasta src

--- src/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "http://localhost:8080",
-});
-axios.defaults.headers.post["Access-Control-Allow-Origin"] = "http://localhost:8080";
-axios.defaults.headers.put["Access-Control-Allow-Origin"] = "http://localhost:8080";
-export default api;
