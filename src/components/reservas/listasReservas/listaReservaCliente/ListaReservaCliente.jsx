0431e4aabc979eeb133d1378ccfb3e9d3cbf7ad9
BeatrizFrancaGusmaoo <beatriz.gusmao@sptech.school>
2023-06-09 10:51:27 -0300

    Alterações na lista reserva cliente

--- src/components/reservas/listasReservas/listaReservaCliente/ListaReservaCliente.jsx
+++ src/components/reservas/listasReservas/listaReservaCliente/ListaReservaCliente.jsx
@@ -39,8 +39,37 @@ function ListaReservaCliente(){
                                 <button className="btnSistema btnMedio" type="submit">Cancelar reserva</button>
                             </div>
                         </div>
+                        <br /><p className='tituloContainer colorWhite' alt='reservas já concluídas'>Reservas já concluídas</p>
+                        <div className="boxLista">
+                                <div className="boxText">
+                                    <b>Dia:</b>
+                                    <p>dd/mm/yyyy</p>
+                                </div>
+                                <div className="boxText">
+                                    <b>Horário:</b>
+                                    <span>00:00 à 00:00</span>
+                                </div>
+                                <div className="boxText">
+                                    <b>Mesa:</b>
+                                    <p>0</p>
+                                </div>
+                                <div className="boxText">
+                                    <b>Quantidade pessoas:</b>
+                                    <p>0</p>
+                                </div>
+                                <div className="boxText">
+                                    <b>Nome estabelecimento:</b>
+                                    <p>Nome</p>
+                                </div>
+                                <div className="boxText">
+                                    <b>Endereço:</b>
+                                    <p>Endereço</p>
+                                </div>
+                            <div className="boxBotao">
+                                <button className="btnSistema btnMedio" type="submit">Realizar feedback</button>
+                            </div>
+                        </div>
                     </div>
-
                 </div>
             </div>
         </section>
