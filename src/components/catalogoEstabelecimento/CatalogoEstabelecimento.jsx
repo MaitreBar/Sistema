d1d9e484248d0170cc31f3bf2ace5b721be9c2e8
BeatrizFrancaGusmaoo <beatriz.gusmao@sptech.school>
2023-06-09 10:45:57 -0300

    Ajustando catálogo estabelecimento

--- src/components/catalogoEstabelecimento/CatalogoEstabelecimento.jsx
+++ src/components/catalogoEstabelecimento/CatalogoEstabelecimento.jsx
@@ -24,34 +24,11 @@ function CatalogoEstabelecimento(){
                                 </div>
                                 <div className="containerSituacaoEstabelecimento">
                                     <img src="" alt="" />
-                                    <b>0.0</b>
+                                    <b className="fontOld">0.0</b>
                                     <p>Capacidade: </p>
                                     <b>Lotação: </b>
                                     <p>De R$ a R$</p>
-                                    <button className="btnSistema btnMedio" type="submit">Cancelar reserva</button>
-                                </div>
-                        </div>
-                        <div className="boxLista">
-                                <div className="containerImagemListaEstabelecimento">
-                                    <img src={imagemEstabelecimento} alt="" />
-                                </div>
-                                <div className="containerDetalheEstabelecimento">
-                                    <b>Nome</b>
-                                    <p>Endereço: </p>
-                                    <br/>
-                                    <b>Faixa de preço: </b>
-                                    <p>De R$ a R$</p>
-                                    <div className="containerTags">
-
-                                    </div>
-                                </div>
-                                <div className="containerSituacaoEstabelecimento">
-                                    <img src="" alt="" />
-                                    <b>0.0</b>
-                                    <p>Capacidade: </p>
-                                    <b>Lotação: </b>
-                                    <p>De R$ a R$</p>
-                                    <button className="btnSistema btnMedio" type="submit">Cancelar reserva</button>
+                                    <button className="btnSistema btnMedio" type="submit">Reservar</button>
                                 </div>
                         </div>
                     </div>
