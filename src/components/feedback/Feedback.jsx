26946d62513fdd5d7f6afc7b0a70303f171bc306
BeatrizFrancaGusmaoo <beatriz.gusmao@sptech.school>
2023-06-09 11:07:26 -0300

    Tela de feedback

--- src/components/feedback/Feedback.jsx
+++ src/components/feedback/Feedback.jsx
@@ -1,8 +1,24 @@
+import "./Feedback.modules.css";
+
 function Feedback(){
     return(
         <section>
-            n
-        </section>
+        <div className='backgroundBody'>            
+            <div className='containerMarromEscuro'>
+                <p className='tituloContainer colorWhite' alt='Login'>Deixe sua avaliação</p>
+                <div className="containerLista">
+                    <div className="boxLista">
+                        <p>Olá, <b>nome cliente</b></p>
+                        <p>Queremos saber mais sobre sua experiência!</p>
+                        <p>Como foi sua reserva no <b>nome restaurante</b>?</p>
+                        <input type="text" className="formularioInput" placeholder="Escreva aqui sua avaliação..." />
+                        <p>Sua opnião é muito importante para continuarmos melhorando nossos serviços! Agradecemos seu feedback!</p>
+                        <button type="submit" className="btnSistema btnMedio">Enviar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
     );
 }
 export default Feedback;
