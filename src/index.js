e74852d3839493932599f2abe7a29fb5441479eb
cesarLimaz29 <cesa.de.sousa.lima@accenture.com>
2023-06-14 09:53:55 -0300

    Calibrando VLibras

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import VLibras from "@djpfs/react-vlibras";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-  <VLibras forceOnload={true} />
+  <VLibras forceOnload={true}/>
     <App />
   </BrowserRouter>
   
