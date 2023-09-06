e44049ae9691d9de363f5e0a9e97cbbb9b442a88
landineo <lucas.rodrigues@sptech.school>
2023-09-06 01:31:33 -0300

    alterando endereço de conexão com api para aws

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "http://52.73.201.125:8080",
 });
-axios.defaults.headers.post["Access-Control-Allow-Origin"] = "http://localhost:8080";
-axios.defaults.headers.put["Access-Control-Allow-Origin"] = "http://localhost:8080";
+axios.defaults.headers.post["Access-Control-Allow-Origin"] = "http://52.73.201.125:8080";
+axios.defaults.headers.put["Access-Control-Allow-Origin"] = "http://52.73.201.125:8080";
 export default api;
